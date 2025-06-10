e151a5a67c30b889a5398b43e437ead3fa1153d1
RedLightning95 <k.danilka@yandex.ru>
2025-06-10 23:46:41 +0700

    Add files via upload

--- exercise_sem+conf_inter.py
+++ exercise_sem+conf_inter.py
@@ -5,22 +5,28 @@ y = '102 91 99 100 103 98 99 101 106 93 103 97 103 101 101 91 104 105 105 100 10
 data = list(map(int, y.split()))
 arr = pd.Series(data) # приводим набор чисел к удобному для нас формату (series/серии)
 
+print('Задание')
 sem = arr.sem() # по заданию необходимо найти стандартную ошибку 95%-го доверительного интервала
 print(f'se: {arr.sem(): .2f}\n95% доверительный интервал = {arr.mean() - arr.sem() * 1.96: .2f} : {arr.mean() + arr.sem() * 1.96: .2f}')
 min_of_conf_interval = arr.mean() - arr.sem() * 1.96 
 max_of_conf_interval = arr.mean() + arr.sem() * 1.96 # находим min и max значения доверительного интервала
 
 # далее идет необязательная, но интересная часть, где мы пытаемся 
-# найти такую случайную выборку, при которой среднее значение ГС будет вне доверительного 95-ти % интервала выборки, а также кол-во попыток
+# найти такую случайную выборку от ГС (мы примем то, что записано под 'y' за ГС), при которой среднее значение ГС будет вне доверительного 95-ти % интервала выборки,
+# а также кол-во попыток
+print()
+print('доп:')
 count = 0
 rand_from_data = r.sample(data, 31)
 arr_rand = pd.Series(rand_from_data)
-while min_of_conf_interval < arr_rand.mean() < max_of_conf_interval:
+min_of_conf_interval_rand = arr_rand.mean() - arr_rand.sem() * 1.96 
+max_of_conf_interval_rand = arr_rand.mean() + arr_rand.sem() * 1.96
+while min_of_conf_interval_rand < arr.mean() < max_of_conf_interval_rand:
     rand_from_data = r.sample(data, 31)
     arr_rand = pd.Series(rand_from_data)
+    min_of_conf_interval_rand = arr_rand.mean() - arr_rand.sem() * 1.96 
+    max_of_conf_interval_rand = arr_rand.mean() + arr_rand.sem() * 1.96
     count += 1
-    if min_of_conf_interval > arr_rand.mean() or arr_rand.mean() > max_of_conf_interval:
-        print('найдена выборка, доверительный интервал которой, не включает в себя среднее ГС')
-        print(f'кол-во попыток найти ее = {count}')
-        print(f'ее среднее - {arr_rand.mean(): .2f}')
-
+    if min_of_conf_interval_rand > arr.mean() or arr.mean() > max_of_conf_interval_rand:
+        print(f'найдена выборка, в доверительный интервал которой не входит mean от ГС\nкол-во попыток на ее нахождение: {count}\nее свойства:\n\tmean: {arr_rand.mean(): .2f}\n\tsem: {arr_rand.sem(): .2f}\n\tmin: {min_of_conf_interval_rand:.2f}\n\tmax: {max_of_conf_interval_rand:.2f}')
+        
